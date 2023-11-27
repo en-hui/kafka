2d9309de9deb86d63695bad66f70751e7b8fba1d
胡恩会 <enhuihu@qq.com>
2023-11-27 23:00:13 +0800

    log

--- a-heh-debug/heh-debug-gradle/src/main/java/com/enhui/ProducerDebuger.java
+++ a-heh-debug/heh-debug-gradle/src/main/java/com/enhui/ProducerDebuger.java
@@ -13,17 +13,9 @@ import org.apache.kafka.common.serialization.StringSerializer;
 public class ProducerDebuger {
 
   public static void main(String[] args) throws ExecutionException, InterruptedException {
-    log.info("start debug producer");
+      Properties properties = initProducerConfig();
 
-    Properties properties = new Properties();
-    properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-debug:9092");
-    properties.setProperty(
-        ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-    properties.setProperty(
-        ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-    properties.setProperty(ProducerConfig.CLIENT_ID_CONFIG, "heh-producer-debug");
-
-    try (KafkaProducer<String, String> producer = new KafkaProducer<>(properties)) {
+      try (KafkaProducer<String, String> producer = new KafkaProducer<>(properties)) {
       String topic = "test1";
       final ProducerRecord<String, String> msg = new ProducerRecord<>(topic, "key", "value");
 
@@ -33,7 +25,7 @@ public class ProducerDebuger {
             if (exception != null) {
               exception.printStackTrace();
             }
-            log.info("异步 callback: metadata: {}", metadata);
+            log.info("debug producer::异步 callback: metadata: {}", metadata);
           });
 
       producer
@@ -43,9 +35,20 @@ public class ProducerDebuger {
                 if (exception != null) {
                   exception.printStackTrace();
                 }
-                log.info("同步 callback: metadata: {}", metadata);
+                log.info("debug producer::同步 callback: metadata: {}", metadata);
               })
           .get();
     }
   }
+
+    private static Properties initProducerConfig() {
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-debug:9092");
+        properties.setProperty(
+            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(
+            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.CLIENT_ID_CONFIG, "heh-producer-debug");
+        return properties;
+    }
 }
